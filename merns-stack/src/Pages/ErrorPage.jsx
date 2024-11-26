f0a614d3bbfb4dac0f94120cda92bccb5a21168f
Solomon Dzokoto <dzokotosolomon85@gmail.com>
2024-11-26 08:18:12 +0000

    Delete merns-stack directory

--- merns-stack/src/Pages/ErrorPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const ErrorPage = () => {
-  console.log("ErrorPage rendered");
-  return <div>Oops! Something went wrong. This page does not exist.</div>;
-}
-
-export default ErrorPage
